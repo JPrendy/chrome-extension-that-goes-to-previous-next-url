618fabc1904c8964a26481d3d2234ba2df85d735
James Prendergast <jamprendergast@gmail.com>
2019-09-08 18:58:19 +0100

    the addition of the Chrome Extension that allows you go to the next or previous url for examples like Jenkins and Jira

---
+++ popup.js
@@ -0,0 +1,27 @@
+let downButton = document.getElementById('down');
+let upButton = document.getElementById('up');
+
+upButton.onclick = function(element) {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+        var result = url.substring(0, url.length-1);
+        //console.log(result);
+        var url2 = result.replace(/\d+$/, function(s) {
+            return +s+1;
+        });
+        chrome.tabs.update(tabs.id, {url: url2});
+    });
+};
+
+
+downButton.onclick = function(element) {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+        var result = url.substring(0, url.length-1);
+        //console.log(result);
+        var url2 = result.replace(/\d+$/, function(s) {
+            return +s-1;
+        });
+        chrome.tabs.update(tabs.id, {url: url2});
+    });
+};
