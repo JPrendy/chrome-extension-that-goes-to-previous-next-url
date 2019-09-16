9ee564d7ae8f36d55e69772bf8395c2960d23352
James Prendergast <jamprendergast@gmail.com>
2019-09-16 22:21:48 +0100

    updated so it will work both in Jenkins and Jira

--- popup.js
+++ popup.js
@@ -4,12 +4,24 @@ let upButton = document.getElementById('up');
 upButton.onclick = function(element) {
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         var url = tabs[0].url;
+        var checkForwardSlash = url.slice(-1); 
+        if(checkForwardSlash != "/"){
+        //console.log(result);
+        var url2 = url.replace(/\d+$/, function(s) {
+            return +s+1;
+                });
+        chrome.tabs.update(tabs.id, {url: url2});
+
+
+        }
+        else{
         var result = url.substring(0, url.length-1);
         //console.log(result);
         var url2 = result.replace(/\d+$/, function(s) {
             return +s+1;
         });
         chrome.tabs.update(tabs.id, {url: url2});
+        }
     });
 };
 
@@ -17,11 +29,23 @@ upButton.onclick = function(element) {
 downButton.onclick = function(element) {
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         var url = tabs[0].url;
+        var checkForwardSlash = url.slice(-1); 
+        if(checkForwardSlash != "/"){
+        //console.log(result);
+        var url2 = url.replace(/\d+$/, function(s) {
+            return +s-1;
+                });
+        chrome.tabs.update(tabs.id, {url: url2});
+
+
+        }
+        else{
         var result = url.substring(0, url.length-1);
         //console.log(result);
         var url2 = result.replace(/\d+$/, function(s) {
             return +s-1;
         });
         chrome.tabs.update(tabs.id, {url: url2});
+        }
     });
 };
